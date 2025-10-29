3165862370da0958316ba4071b4025eb8e262470
iocchi <iocchi@diag.uniroma1.it>
2025-10-29 12:28:03 +0100

    start

---
+++ docker/start.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SESSION=0
+
+CMD="docker exec -it marrtino2_gz "
+
+$CMD tmux rename-window -t $SESSION:0 'robot'
+$CMD tmux new-window -t $SESSION:1 -n 'http'
+$CMD tmux new-window -t $SESSION:2 -n 'ws'
+$CMD tmux new-window -t $SESSION:3 -n 'world'
+
+$CMD tmux send-keys -t $SESSION:0 'cd ~/src/marrtino_gazebo/bin' C-m
+$CMD tmux send-keys -t $SESSION:0 './smarrtino.bash' C-m
+
+sleep 3
+
+$CMD tmux send-keys -t $SESSION:1 'cd ~/src/marrtino_gazebo/bin' C-m
+$CMD tmux send-keys -t $SESSION:1 './start_http.bash' C-m
+
+sleep 3
+
+$CMD tmux send-keys -t $SESSION:2 'cd ~/src/marrtino_gazebo/bin' C-m
+$CMD tmux send-keys -t $SESSION:2 './start_codeserver.bash' C-m
+
+sleep 3
+
+$CMD tmux send-keys -t $SESSION:3 'cd ~/src/marrtino_gazebo/marrtino_gazebo/src' C-m
+$CMD tmux send-keys -t $SESSION:3 'python gz_objects.py -a objs.conf' C-m
+
+sleep 3
+
+google-chrome-stable http://localhost:8000/code.html &
+
+echo "Done"
+
+
